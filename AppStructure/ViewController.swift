02c35602b9ff363e4da1746a3419a3bbf7174c3f
Abdelrady <ahmed.abdelrady@ibtikar.net.sa>
2022-03-15 12:51:45 +0200

    Initial Commit

---
+++ AppStructure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStructure
+//
+//  Created by Ahmad Abdulrady on 15/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
