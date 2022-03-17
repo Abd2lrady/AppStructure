13334967af2c10f0c68ec397605b609678a7cbd0
Abdelrady <ahmed.abdelrady@ibtikar.net.sa>
2022-03-17 13:48:50 +0200

    making pods deployment target ios 12 due to compatiblity issue

--- AppStructure/Scenes/ViewController.swift
+++ AppStructure/Scenes/ViewController.swift
@@ -6,15 +6,17 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
 class ViewController: UIViewController {
-
+    var indicator: NVActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .green
         testLabel()
-     
+        testIndicator()
+        indicator?.startAnimating()
     }
     
     func testLabel() {
@@ -25,6 +27,10 @@ class ViewController: UIViewController {
                                      label.centerYAnchor.constraint(equalTo: view.centerYAnchor)])
         label.text = NSLocalizedString(Localization.title, comment: "")
         label.font = UIFont(font: Fonts._29LTAzer.bold, size: 20)
-        
+    }
+    
+    func testIndicator() {
+        indicator = NVActivityIndicatorView(frame: view.frame, type: .ballClipRotate, color: .black, padding: 20)
+        view.addSubview(indicator)
     }
 }
