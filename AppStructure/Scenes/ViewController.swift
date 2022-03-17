dbe0f9ed6a4e608f61630a28a368a2f38edc9fe2
Abdelrady <ahmed.abdelrady@ibtikar.net.sa>
2022-03-17 13:15:16 +0200

    adding arabic localiztion

--- AppStructure/Scenes/ViewController.swift
+++ AppStructure/Scenes/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .green
+        testLabel()
+     
     }
-
+    
+    func testLabel() {
+        let label = UILabel()
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                                     label.centerYAnchor.constraint(equalTo: view.centerYAnchor)])
+        label.text = NSLocalizedString(Localization.title, comment: "")
+        label.font = UIFont(font: Fonts._29LTAzer.bold, size: 20)
+        
+    }
 }
