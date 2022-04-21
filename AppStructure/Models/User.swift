a67686bf392006f4eacb834dc39bc9bac81e875b
Abd2lrady <abd2lrady@gmail.com>
2022-04-21 05:30:04 +0200

    adding user model

---
+++ AppStructure/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  AppStructure
+//
+//  Created by Ahmad Abdulrady
+//
+
+import Foundation
+
+struct User {
+    let id: String
+    let email: String
+    let password: String
+    let firstName: String?
+    let lastName: String?
+}
