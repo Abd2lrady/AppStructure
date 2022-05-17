a3afc779d31b4881ada44c0031c07155266587bc
Abd2lrady <abd2lrady@gmail.com>
2022-05-17 16:11:19 +0200

    Adding CodingKeys Enum to user model

--- AppStructure/Models/User.swift
+++ AppStructure/Models/User.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 
-struct User {
+struct User: Codable {
     let id: String
     let email: String
     let password: String
     let firstName: String?
     let lastName: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, email, password, firstName, lastName
+    }
+    
 }
+
+
